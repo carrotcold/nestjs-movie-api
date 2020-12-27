f5d198ad480cd40b141876cf9c5027e3cce5f3db
Soin Na <carrotcold@gmail.com>
2020-12-28 04:54:38 +0900

    add dto(create, update) and validator

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,7 +1,6 @@
 export class Movie {
   id: number;
   title: string;
-  director: string;
   year: number;
   genres: string[];
 }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,8 +6,9 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -21,22 +22,24 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
+    // param으로 넘어온 값은 기본적으로 string이지만
+    // validation pipe에 의해 number로 transform
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
